338f0d30599bd715daa6510820625e9953b2f7f1
Mariana Calmon <mariana.calmon@wolox.com.ar>
2023-02-02 10:16:09 -0300

    First commit, project configuration.

--- wbooks/src/constants/mockComments.ts
+++ wbooks/src/constants/mockComments.ts
@@ -2,8 +2,7 @@ export const COMMENTS_MOCK = [
   {
     id: 0,
     author: 'Emmie Thiel',
-    text:
-      'It was great to see you again earlier. Let’s definitely get that coffe It was great to see you again earlier.',
+    text: 'It was great to see you again earlier. Let’s definitely get that coffe It was great to see you again earlier.',
     url: null
   },
   {
