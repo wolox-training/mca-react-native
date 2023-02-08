6a0f3ab35025d3e2b5269c11b2c6306b267d9d0c
Mariana Calmon <mariana.calmon@wolox.com.ar>
2023-02-08 17:17:13 -0300

    Book list created.

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,24 +1,46 @@
+import { COMMENTS_MOCK } from '@constants/mockComments';
 import React from 'react';
-import { SafeAreaView, ScrollView, View, StatusBar } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  StatusBar,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  Image
+} from 'react-native';
 
+import bell from './assets/bell.png';
+import search from './assets/search.png';
 import Book from './components/Book';
 import styles from './styles';
 
 const App = () => {
+  const onPress = () => {
+    console.log('Hola');
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
           <View>
+            <View style={styles.sectionTitle}>
+              <TouchableOpacity onPress={onPress}>
+                <Image source={bell} />
+              </TouchableOpacity>
+              <Text style={styles.title}>Library</Text>
+              <TouchableOpacity onPress={onPress}>
+                <Image source={search} />
+              </TouchableOpacity>
+            </View>
             <View style={styles.sectionContainer}>
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
-              <Book title={'A little bird told me'} author={'Timothy Cross'} url={0} />
+              <FlatList
+                data={COMMENTS_MOCK}
+                renderItem={({ item }) => <Book title={item.text} author={item.author} url={item.url} />}
+                keyExtractor={item => item.id.toString()}
+              />
             </View>
           </View>
         </ScrollView>
--- wbooks/src/app/styles.ts
+++ wbooks/src/app/styles.ts
@@ -5,6 +5,21 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: COLORS.aliceblue
   },
+  sectionTitle: {
+    alignItems: 'center',
+    backgroundColor: COLORS.deepSkyBlue,
+    display: 'flex',
+    flexDirection: 'row',
+    height: 84,
+    justifyContent: 'space-between',
+    padding: 15
+  },
+  title: {
+    color: COLORS.white,
+    fontSize: 17,
+    fontWeight: 'bold',
+    textTransform: 'uppercase'
+  },
   sectionContainer: {
     marginTop: 35,
     paddingHorizontal: 20
